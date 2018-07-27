c90ea1d612e3d1a1df6c40cb24c0d899978263ff
gautam <gautamaggarwal1989@gmail.com>
2018-07-27 15:02:31 +0530

    A minor change according to pep8

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ CUSTOMER_SECRET = ''
 AUTH_TOKEN = ''
 AUTH_SECRET_KEY = ''
 
-TWEET_PER_PAGE = 200 # This limit has been put by Twitter.
+TWEET_PER_PAGE = 200   # This limit has been put by Twitter.
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 FALL_BACK_TIME = 60
